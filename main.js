99b91fef5eee23c1899181499c5552d0d4e0fa99
taka.umada@hotmail.com <taka.umada@hotmail.com>
2014-08-04 11:30:19 +0000

    second commit

---
+++ main.js
@@ -0,0 +1,13 @@
+/// <reference path="typings/jquery/jquery.d.ts" />
+/// <reference path="typings/d3/d3.d.ts" />
+
+
+var msg = 'hello monaco'
+var i = 10;
+var s = "string";
+
+
+window.onload = function () {
+	window.alert(msg);
+}
+
